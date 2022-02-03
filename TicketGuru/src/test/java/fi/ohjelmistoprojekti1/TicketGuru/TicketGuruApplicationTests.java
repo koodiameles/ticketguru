4a292454b4af026e1c13d56e3fdd1cf8c0f3a750
Mirka Heikkila <mirkanms@hotmail.com>
2022-02-03 18:43:33 +0200

    aloita springboot

---
+++ TicketGuru/src/test/java/fi/ohjelmistoprojekti1/TicketGuru/TicketGuruApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.ohjelmistoprojekti1.TicketGuru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketGuruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
