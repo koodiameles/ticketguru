105780f012b8960774021528d8630890f6eb3503
Arponen <max.arponen@gmail.com>
2022-02-15 23:51:50 +0200

    Add Role and Role repository

---
+++ TicketGuru/src/main/java/fi/ohjelmistoprojekti1/TicketGuru/domain/Role.java
@@ -0,0 +1,63 @@
+package fi.ohjelmistoprojekti1.TicketGuru.domain;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Role {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long roleid;
+	private String description;
+	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "employee")
+	private List<Employee> employees;
+	
+	
+
+	public Role(String description) {
+		super();
+		this.description = description;
+	}
+
+	public Long getroleid() {
+		return roleid;
+	}
+
+	public void setEmployeeid(Long roleid) {
+		this.roleid = roleid;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	public List<Employee> getEmployees() {
+		return employees;
+	}
+
+	public void setEmployees(List<Employee> employees) {
+		this.employees = employees;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Role [roleid=" + roleid + ", description=" + description +  ", employees="
+				+ employees + "]"; 
+	}	
+}
---
+++ TicketGuru/src/main/java/fi/ohjelmistoprojekti1/TicketGuru/domain/RoleRepository.java
@@ -0,0 +1,10 @@
+package fi.ohjelmistoprojekti1.TicketGuru.domain;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface RoleRepository extends CrudRepository<Role, Long> {
+	
+
+}
