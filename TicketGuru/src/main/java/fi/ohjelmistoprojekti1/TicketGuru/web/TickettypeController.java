997148bb7ef39e6358fe871ce5704bb434501435
Mirka Heikkila <mirkanms@hotmail.com>
2022-03-13 19:37:41 +0200

    Add validtation to tickettype Entity and responseStatus to tickettypeController

--- TicketGuru/src/main/java/fi/ohjelmistoprojekti1/TicketGuru/web/TickettypeController.java
+++ TicketGuru/src/main/java/fi/ohjelmistoprojekti1/TicketGuru/web/TickettypeController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,10 +13,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.server.ResponseStatusException;
 
+import fi.ohjelmistoprojekti1.TicketGuru.domain.Event;
 import fi.ohjelmistoprojekti1.TicketGuru.domain.EventRepository;
 import fi.ohjelmistoprojekti1.TicketGuru.domain.Tickettype;
 import fi.ohjelmistoprojekti1.TicketGuru.domain.TickettypeRepository;
+
 
 @RestController
 public class TickettypeController {
@@ -29,13 +33,23 @@ public class TickettypeController {
 	// Get one tickettype
 	@GetMapping("/tickettypes/{id}")
 	public Optional<Tickettype> findTickettypeRest(@PathVariable("id") Long tickettypeid) {
-		return tickettyperepository.findById(tickettypeid);
+	Optional <Tickettype> tickettypeResult = tickettyperepository.findById(tickettypeid); 
+	if (!tickettypeResult.isPresent()) {
+		throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Tickettype id is not valid");
+		}
+	return tickettyperepository.findById(tickettypeid); 
 	}
 	
 	// Get all tickettypes
 	@GetMapping("/tickettypes")
-	public List<Tickettype> tickettypeListRest() {
-		return tickettyperepository.findAll();
+	public ResponseEntity<List<Tickettype>> getAllTickettypes() {
+		List<Tickettype>list=(List<Tickettype>) tickettyperepository.findAll(); 
+		if(list.isEmpty()) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "There are no tickettypes"); 
+		} else {
+			return new ResponseEntity<>(list, HttpStatus.OK); 
+		}
+		
 	}
 	
 	// Add (POST) a new event
