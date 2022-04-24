83dc3bbd5527936703bb2ac0259c2f4707b33aca
Jussi Junnila <74765606+koodiameles@users.noreply.github.com>
2022-04-24 11:37:49 +0300

    minor cleanup fixes

--- TicketGuru/src/main/java/fi/ohjelmistoprojekti1/TicketGuru/web/ClientController.java
+++ TicketGuru/src/main/java/fi/ohjelmistoprojekti1/TicketGuru/web/ClientController.java
@@ -1,12 +1,7 @@
 package fi.ohjelmistoprojekti1.TicketGuru.web;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import fi.ohjelmistoprojekti1.TicketGuru.domain.TicketRepository;
-
-import org.springframework.ui.Model;
 
 @Controller
 public class ClientController {
@@ -19,7 +14,7 @@ public class ClientController {
 
 	 // GET SELLTICKETS - 
 	 @GetMapping(value={"/selltickets"})
-	 public String getSellTickets (Model model) {
+	 public String getSellTickets () {
 		 return "selltickets";
 	 }
     
