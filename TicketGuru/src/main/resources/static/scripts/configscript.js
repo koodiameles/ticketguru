29ee23861f073d51baf3f94c3dba34f7a586e6b5
Jussi Junnila <74765606+koodiameles@users.noreply.github.com>
2022-05-04 18:09:03 +0300

    fix typos

--- TicketGuru/src/main/resources/static/scripts/configscript.js
+++ TicketGuru/src/main/resources/static/scripts/configscript.js
@@ -3,11 +3,11 @@
 /* global variables*/
 
 var globalvariable = {
-    url: 'https://ticketguru22.herokuapp.com/',
+    g_url: 'https://ticketguru22.herokuapp.com/',
     // g_url: "http://localhost:8080/",
 
     g_pass: btoa("admin:admin"),
-    // var pass: btoa("user:user"),
+    // g_pass: btoa("user:user"),
 
     g_msg: "",
 
