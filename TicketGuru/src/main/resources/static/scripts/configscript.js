9bebdb6efb25c979ab53f1a673ad91ee175423c5
Jussi Junnila <74765606+koodiameles@users.noreply.github.com>
2022-05-04 17:28:44 +0300

    chance variables for herokupush

--- TicketGuru/src/main/resources/static/scripts/configscript.js
+++ TicketGuru/src/main/resources/static/scripts/configscript.js
@@ -3,8 +3,8 @@
 /* global variables*/
 
 var globalvariable = {
-    // url: 'https://ticketguru22.herokuapp.com/',
-    g_url: "http://localhost:8080/",
+    url: 'https://ticketguru22.herokuapp.com/',
+    // g_url: "http://localhost:8080/",
 
     g_pass: btoa("admin:admin"),
     // var pass: btoa("user:user"),
