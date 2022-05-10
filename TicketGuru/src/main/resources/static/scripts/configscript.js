924f019d1e42b8750d2590322e483b67721554fd
Jussi Junnila <74765606+koodiameles@users.noreply.github.com>
2022-05-10 19:00:17 +0300

    merge with main to get few changes that were made directly there. Chance variables back for local development

--- TicketGuru/src/main/resources/static/scripts/configscript.js
+++ TicketGuru/src/main/resources/static/scripts/configscript.js
@@ -3,8 +3,8 @@
 /* global variables*/
 
 var globalvariable = {
-    g_url: 'https://ticketguru22.herokuapp.com/',
-    // g_url: "http://localhost:8080/",
+   //  g_url: 'https://ticketguru22.herokuapp.com/',
+    g_url: "http://localhost:8080/",
 
     g_pass: btoa("admin:admin"),
     // g_pass: btoa("user:user"),
